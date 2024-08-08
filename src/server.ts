19443907fcbbee7c2b30f534d619ece9fd7fcef3
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-08-08 19:47:25 +0700

    Update CORS config and auth routes

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,32 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes";
 import { dbClient, dbConn } from "../db/client";
 import { users, stores, tables, favorites, reservations } from "../db/schema";
 
+dotenv.config();
+
 const app = express();
-const port = 3000;
-
 app.use(bodyParser.json());
-app.use(cors());
 
+const corsOptions = {
+  origin: "http:localhost:5173",
+};
+app.use(cors(corsOptions));
+
+
+app.use("/api/auth", authRoutes);
+//POST: localhost:3000/api/auth/register {name, email, password, role}
+//POST: localhost:3000/api/auth/login {email, password}
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "JongRhanRhao backend is up and running!" });
+});
+
+const port = process.env.BACKEND_PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
