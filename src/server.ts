b02cabdbaf2eebdb644ba91874b60959892e640a
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 00:06:52 +0700

    chore: Update server connection log

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,7 @@ dotenv.config();
 
 const port = process.env.BACKEND_PORT || 3000;
 app.listen(port, () => {
-  console.log(`🍺 Server is running on ${process.env.SERVER_URL}:${port}`);
+  console.log(
+    `🍺 JongRhanRhao server is running on ${process.env.SERVER_URL}:${process.env.SERVER_PORT}`
+  );
 });
