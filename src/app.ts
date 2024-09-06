1ba8bd023ece2ee2751e218260ff39c864d52156
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-06 23:56:11 +0700

    chore: Add initial setup for Express server and routes

---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import passport from "passport";
+import session from "express-session";
+import helmet from "helmet";
+
+import "./config/passport";
+import auth from "./routes/auth";
+import jong from "./routes/jong";
+
+dotenv.config();
+
+export const app = express();
+
+const corsOptions = {
+  origin: "localhost:5173",
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "tanned-catchable-spool",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/users/auth", auth);
+app.use("/stores/api/", jong);
+app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "JongRhanRhao backend is up and running!" });
+});
