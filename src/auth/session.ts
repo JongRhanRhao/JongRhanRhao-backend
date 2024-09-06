116ba68ae44f881316d0bfc21ba9400c5887f323
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 03:14:03 +0700

    feat: Add session configuration in session.ts

---
+++ src/auth/session.ts
@@ -0,0 +1,13 @@
+import session from "express-session";
+import { NODE_ENV } from "../utils/env";
+
+export const sessionInstance = session({
+  secret: process.env.SESSION_SECRET || "tanned-catchable-spool",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    secure: NODE_ENV === "production" ? true : false,
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+});
