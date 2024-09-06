c094f78b3ebf50702ef761d020a16c098d6b0ccd
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 00:03:27 +0700

    refactor: Update connection log

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,5 @@ dotenv.config();
 
 const port = process.env.BACKEND_PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`🍺 Server is running on ${process.env.SERVER_URL}:${port}`);
 });
