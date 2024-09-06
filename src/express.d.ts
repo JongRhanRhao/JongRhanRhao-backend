5db6f7aa97a06953bdd314a15fdfdc1e5c6ae473
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-06 23:56:22 +0700

    chore: Remove unnecessary database configuration files and routes

--- src/express.d.ts
+++
@@ -1,9 +0,0 @@
-import * as multer from 'multer';
-
-declare global {
-  namespace Express {
-    interface Request {
-      files?: multer.File[];  // Declare `files` as an array of `multer.File`
-    }
-  }
-}
