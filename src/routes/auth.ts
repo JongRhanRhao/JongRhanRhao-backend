1d246b9ed48ab2cf502ce7640bb4f5119e7771dd
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-07 17:27:53 +0700

    refactor: Update auth routes in auth.ts

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,5 @@
 import passport from "passport";
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import { register } from "../controller/authController";
@@ -10,12 +10,42 @@ router.post(
   "/login",
   passport.authenticate("local", { failureRedirect: "/login" }),
   function (req, res) {
+    res.json({
+      message: "Login successful",
+      user: req.user,
+    });
+  }
+);
+
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
     res.redirect("/");
   }
 );
-// router.get("/google", googleAuth);
-// router.get("/google/callback", googleAuthCallback);
+
 // router.get("/facebook", facebookAuth);
 // router.get("/facebook/callback", facebookAuthCallback);
+router.get("/sessions", (req, res) => {
+  if (req.isAuthenticated() && req.user) {
+    res.json({ user: req.user });
+  } else {
+    res.status(401).json({ message: "No active session" });
+  }
+});
+
+router.get("/me", (req: Request, res: Response) => {
+  if (req.isAuthenticated()) {
+    res.status(200).json({
+      userId: req.user?.userId,
+      userName: req.user?.userName,
+      userEmail: req.user?.userEmail,
+    });
+  } else {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+});
 
 export default router;
