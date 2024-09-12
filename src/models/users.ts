93d4e50438e4902773f7305494dbb985a5d9db6d
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-12 13:21:34 +0700

    refactor: Update user model to include phoneNumber, googleId, and facebookId fields

--- src/models/users.ts
+++ src/models/users.ts
@@ -2,9 +2,8 @@ export interface User {
   userId: string;
   userName: string;
   userEmail: string;
-  password: string | null;
   role: string;
-  phoneNumber: string | null;
-  googleId: string | null;
-  facebookId: string | null;
-  }
+  phoneNumber: string;
+  googleId: string;
+  facebookId: string;
+}
