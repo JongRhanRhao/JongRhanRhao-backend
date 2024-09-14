fc5f028e5e398758dc6ac65983908be974cd2245
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-15 03:24:40 +0700

    refactor: Update reservationController to use string type for id parameter

--- src/controller/reservationController.ts
+++ src/controller/reservationController.ts
@@ -146,33 +146,23 @@ export const createReservation = async (req: Request, res: Response) => {
 
 // Update a reservation
 export const updateReservation = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
-  const {
-    tableId,
-    numberOfTable,
-    customerId,
-    reservationTime,
-    numberOfPeople,
-    customerName,
-    phone_number,
-  } = req.body;
+  const id = req.params.id;
+  const { numberOfPeople, reservationTime, reservationStatus, phoneNumber } =
+    req.body;
+
   try {
     const result = await pool.query(
-      "UPDATE reservations SET table_id = $1, number_of_table = $2, customer_id = $3, reservation_time = $4, number_of_people = $5, customer_name = $6, customer_phone = $7 WHERE reservation_id = $8 RETURNING *",
-      [
-        tableId,
-        numberOfTable,
-        customerId,
-        reservationTime,
-        numberOfPeople,
-        customerName,
-        phone_number,
-        id,
-      ]
+      `UPDATE reservations 
+       SET number_of_people = $1, reservation_time = $2, reservation_status = $3, phone_number = $4 
+       WHERE reservation_id = $5 
+       RETURNING *`,
+      [numberOfPeople, reservationTime, reservationStatus, phoneNumber, id]
     );
+
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "Reservation not found" });
     }
+
     res.status(200).json(result.rows[0]);
   } catch (err) {
     console.error("Error updating reservation:", err);
@@ -180,17 +170,39 @@ export const updateReservation = async (req: Request, res: Response) => {
   }
 };
 
+export const updateReservationStatus = async (req: Request, res: Response) => {
+  const { reservationStatus } = req.body;
+  const reservationId = req.params.id;
+
+  try {
+    const result = await pool.query(
+      "UPDATE reservations SET reservation_status = $1 WHERE reservation_id = $2 RETURNING *",
+      [reservationStatus, reservationId]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Reservation not found" });
+    }
+    res.status(200).json(result.rows[0]);
+  } catch (err) {
+    console.error("Error updating reservation status:", err);
+    res.status(500).json({ error: err.message });
+  }
+};
+
 // Delete a reservation
 export const deleteReservation = async (req: Request, res: Response) => {
-  const id = parseInt(req.params.id);
+  const id = req.params.id;
+
   try {
     const result = await pool.query(
       "DELETE FROM reservations WHERE reservation_id = $1 RETURNING *",
       [id]
     );
+
     if (result.rows.length === 0) {
       return res.status(404).json({ error: "Reservation not found" });
     }
+
     res.status(204).json({});
   } catch (err) {
     console.error("Error deleting reservation:", err);
