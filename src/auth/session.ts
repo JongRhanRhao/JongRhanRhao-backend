c55ea7525c6023f424924e47ee5f6191366f35bf
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-09-15 23:46:29 +0700

    refactor: Add sameSite attribute to session configuration

--- src/auth/session.ts
+++ src/auth/session.ts
@@ -9,5 +9,6 @@ export const sessionInstance = session({
     httpOnly: true,
     secure: NODE_ENV === "production" ? true : false,
     maxAge: 1000 * 60 * 60 * 24,
+    sameSite: "strict",
   },
 });
