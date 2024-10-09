8480490c453479417d47e2758c15c4d13e4d0681
Ton Natthapon <nattapon_ton_np@hotmail.com>
2024-10-10 01:09:08 +0700

    refactor: Update import paths and add Swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import passport from "passport";
 import "./auth/passport.js";
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import swaggerOptions from "./swagger.js";
 
 import { NODE_ENV } from "./utils/env.js";
 import auth from "./routes/auth.js";
@@ -13,8 +16,10 @@ import { sessionInstance } from "./auth/session.js";
 dotenv.config();
 
 const app = express();
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
 setupMiddlewares(app);
+
 
 if (NODE_ENV === "production") app.set("trust proxy", 1);
 
@@ -28,6 +33,7 @@ app.use("/users/auth", auth);
 app.use("/users", user);
 app.use("/stores/api", management);
 app.use("/uploads", express.static("uploads"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ message: "JongRhanRhao backend is up and running!" });
